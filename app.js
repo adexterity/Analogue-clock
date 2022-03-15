4e893b20da115f90684f49ba6561f678717eda22
Joseph Suberu <opensesame4me@gmail.com>
2022-03-15 10:44:07 +0100

    new changes
    made new changes here

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 var secondHand = document.querySelector(".");
 var minuteHand = document.querySelector(".");
@@ -15,3 +16,31 @@ function setTime(){
 funtion (element, ){
 
 }
+=======
+
+setInterval( setTime, 1000);
+
+var secondHand = document.querySelector(".second");
+var minuteHand = document.querySelector(".minute");
+var hourHand = document.querySelector(".hour");
+
+function setTime(){
+
+    var currentDate = new Date();
+    var secondsRatio = currentDate.getSeconds() / 60;
+    var minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    var hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+
+
+}
+
+function setRotation (element, rotationRatio ){
+    element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setTime();
+>>>>>>> Stashed changes
