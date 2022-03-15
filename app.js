076a71fb5f8b80e096078e7859bbc4f5923d8854
Joseph Suberu <opensesame4me@gmail.com>
2022-03-15 09:25:40 +0100

    Analogue clock created by me
    this is an analogue clock i design using html, css and javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+
+var secondHand = document.querySelector(".");
+var minuteHand = document.querySelector(".");
+var hourHand = document.querySelector(".");
+
+function setTime(){
+
+    var currentDate = new Date();
+    var seconds = currentDate.getSeconds();
+    var minutes = currentDate.getMinutes();
+    var hours = currentDate.getHours();
+
+}
+
+funtion (element, ){
+
+}
