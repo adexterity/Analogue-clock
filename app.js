5c80a836da3861b3b967f8f7a10c3a5d4f342d7f
Joseph Suberu <opensesame4me@gmail.com>
2022-03-15 10:48:35 +0100

    updated
    another commit

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-<<<<<<< Updated upstream
-
-var secondHand = document.querySelector(".");
-var minuteHand = document.querySelector(".");
-var hourHand = document.querySelector(".");
-
-function setTime(){
-
-    var currentDate = new Date();
-    var seconds = currentDate.getSeconds();
-    var minutes = currentDate.getMinutes();
-    var hours = currentDate.getHours();
-
-}
-
-funtion (element, ){
-
-}
-=======
-
 setInterval( setTime, 1000);
 
 var secondHand = document.querySelector(".second");
