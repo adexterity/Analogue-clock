c3be94819ae7d9249404cc4239f563dda97e0722
adexterity <99201855+adexterity@users.noreply.github.com>
2022-03-18 17:53:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Analogue-clock
+a clock i made using the new javascript date method i learnt...........this helps me to understand the javascript new Date() object constructor, the setInterval() function and more
---
+++ app.js
@@ -0,0 +1,30 @@
+setInterval( setTime, 1000);
+
+var secondHand = document.querySelector(".second");
+var minuteHand = document.querySelector(".minute");
+var hourHand = document.querySelector(".hour");
+
+function setTime(){
+
+    var currentDate = new Date();
+    var secondsRatio = currentDate.getSeconds() / 60;
+    var minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    var hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+
+
+}
+
+function setRotation (element, rotationRatio ){
+    element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setTime();
+
+
+
+
+
